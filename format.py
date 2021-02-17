009b39415646035938ab5b61e4af29a3cc1e18be
Emma Dickson <emmadickson@Emmas-MacBook-Pro.local>
2021-02-17 16:30:46 -0500

    update

--- format.py
+++ format.py
@@ -1,7 +1,7 @@
 import json
 
-formatted_pages = open('pages_formatted.js', 'w')
-pages = open('pages/pages.jsonl', 'r')
+formatted_pages = open('index.js', 'w')
+pages = open('resources/pages.jsonl', 'r')
 
 test = {}
 index = 0
@@ -11,5 +11,6 @@ for line in lines:
         json_line = json.loads(line)
         test[json_line['text']] = [json_line['url'], json_line['title']]
     index = index + 1
-
+formatted_pages.write("var index = ");
 formatted_pages.write(json.dumps(test))
+formatted_pages.write(";");
